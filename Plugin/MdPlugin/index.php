ed9325aaac47440c5713f57e9c4897bfc6471ec8
may <maywanting@gmail.com>
2016-11-01 13:32:53 +0800

    composer based will be added in the next version

--- Plugin/MdPlugin/index.php
+++ Plugin/MdPlugin/index.php
@@ -1,5 +1,7 @@
 <?php namespace Plugin;
 
+require_once __APPDIR__ . '/Extension/Parsedown.php';
+
 class MdPlugin extends Plugin{
 
     //new 命令
@@ -18,6 +20,16 @@ class MdPlugin extends Plugin{
 
     //construct 命令
     public static function construct() {
-        dd("fuck");
+        $fileName = __APPDIR__ . '/source/test/test.md';
+        $htmlName = __APPDIR__ . '/source/test/test.html';
+
+        $markdown = file_get_contents($fileName);
+        $markdown = explode("\n", $markdown);
+
+        $parsedown = new \Parsedown();
+        $html = [];
+        foreach ($markdown as $value) {
+            echo $parsedown->text($value);
+        }
     }
 }
